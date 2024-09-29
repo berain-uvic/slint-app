5ecf99513a7f6551a8566b0c05d98a2d863630f2
Bryan Eriksson <bryaneriksson@gmail.com>
2024-09-28 17:54:15 -0700

    Complete

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-use std::error::Error;
-
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
 
-    // ui.on_request_increase_value({
-    //     let ui_handle = ui.as_weak();
-    //     move || {
-    //         let ui = ui_handle.unwrap();
-    //         ui.set_counter(ui.get_counter() + 1);
-    //     }
-    // });
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+    let ui_handle = ui.as_weak();
+
+    ui.on_divide_income(move |string| {
+        let ui = ui_handle.unwrap();
+        let num: f64 = string.trim().parse().unwrap();
+        let tax: f64 = num * TAXPER;
+        let owner: f64 = num * OWNERPER;
+        let profit: f64 = num * PROFITPER;
+        let opex: f64 = num * OPEXPER;
+        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpeEx: {:.2}", tax, owner, profit, opex);
+        ui.set_results(result.into());
+    });
 
     ui.run()?;
 
